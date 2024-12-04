1a5863d70dd549d99a90f2e374b0f0ba81fcfdc8
Ndrs03 <kaa.alexandersson@outlook.com>
2024-12-04 14:30:06 +0100

    funkar inte än

--- src/main/java/GreetingResource.java
+++ src/main/java/GreetingResource.java
@@ -1,13 +1,17 @@
 
+import jakarta.annotation.Resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jsonStructures.GreetingJson;
 
+import javax.sql.DataSource;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/greeting")
 public class GreetingResource {
+
+
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
--- src/main/java/HelloResource.java
+++ src/main/java/HelloResource.java
@@ -1,16 +1,33 @@
 
+
+import jakarta.annotation.Resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
+import javax.sql.DataSource;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static java.lang.System.out;
 
 @Path("/hello")
 public class HelloResource {
 
+    @Resource(lookup = "jdbc/MySqlPool")
+    DataSource dataSource;
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayHello() {
-        return "Hello World";
+    public String getHello() {
+        try (Connection connection = dataSource.getConnection()) {
+            return ((java.sql.Connection) connection).getMetaData().getDatabaseProductName() + "-" + ((java.sql.Connection) connection).getCatalog();
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return "Error: " + e.getMessage();
+        }
     }
 }
